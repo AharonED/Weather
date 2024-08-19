078cf88c85488d7888d2c9c564de979937de777e
Aharon.Garada <aharon.garada@hubsecurity.io>
2024-08-19 18:13:52 +0300

    Fetch weather data from open-meteo.com

--- backend/weather/data_ingestion/fetch_weather_data.py
+++ backend/weather/data_ingestion/fetch_weather_data.py
@@ -1,11 +1,23 @@
 import requests
 from datetime import datetime
 
-# API URL with your desired parameters
+# We use https://open-meteo.com - Free Weather API, to retrive weather data.
+# Make sure all required weather variables are listed here
+# The order of variables in hourly or daily is important to assign them correctly below
+# url = "https://api.open-meteo.com/v1/forecast"
+# params = {
+# 	"latitude": 52.52,
+# 	"longitude": 13.41,
+# 	"hourly": "temperature_2m"
+# }
+
+# API URL with desired parameters
 API_URL = "https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&past_days={past_days}&hourly={hourly_parameters}"
 
 
 # Function to fetch weather data for a given location
+# Example usage:
+#     weather_data = fetch_weather_data(latitude=52.52, longitude=13.41)
 def fetch_weather_data(
     latitude,
     longitude,
@@ -18,14 +30,9 @@ def fetch_weather_data(
         past_days=past_days,
         hourly_parameters=hourly_parameters,
     )
+
     response = requests.get(url)
     response.raise_for_status()  # Raise an exception for non-200 status codes
 
     data = response.json()
     return data
-
-
-# # Example usage with your location
-# if __name__ == "__main__":
-#     weather_data = fetch_weather_data(latitude=52.52, longitude=13.41)
-#     print(weather_data)  # Example output: (refer to provided data model)
