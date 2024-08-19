8f3bb6fc0b595db79d930ec730883182408720da
Aharon.Garada <aharon.garada@hubsecurity.io>
2024-08-19 23:42:44 +0300

    Add view to run the data fetch from outside event

--- data_service/weather_data_service/views.py
+++ data_service/weather_data_service/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import WeatherEntry
+from .serializers import WeatherEntrySerializer
+import asyncio
+from .async_tasks import main
+
+
+class WeatherViewSet(viewsets.ViewSet):
+    # @api_view(["GET"])
+    @permission_classes([IsAuthenticated])
+    def list(self, request):  # /api/weather
+        asyncio.run(main())
+        weather = WeatherEntry.objects.all()
+        serializer = WeatherEntrySerializer(instance=weather, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
