4c9d2f9a4dd7e9d387013f95ff273372783f9095
Aharon.Garada <aharon.garada@hubsecurity.io>
2024-08-19 23:41:51 +0300

    add data fwtch and process handling

---
+++ data_service/weather_data_service/async_tasks.py
@@ -0,0 +1,20 @@
+import asyncio
+import time
+from .fetch_weather_data import fetch_weather_data
+from .models import WeatherEntry
+from .data_processing import process_and_store_data
+
+
+async def fetch_and_process_data(days):
+    data = fetch_weather_data(past_days=days)
+    process_and_store_data(data)
+
+
+async def main():
+    tasks = []
+    for days in range(1, 1, 10):
+        print(f"fetch_and_process_data({days})")
+        tasks.append(asyncio.create_task(fetch_and_process_data(days)))
+        print(f"tasks.append(create_task(fetch_and_process_data({days})))")
+
+    await asyncio.gather(*tasks)
---
+++ data_service/weather_data_service/data_processing.py
@@ -0,0 +1,56 @@
+from django.db.models import Avg
+from .models import WeatherEntry, DailyWeatherSummary
+from datetime import datetime
+
+
+# Function to process and store fetched data
+def process_and_store_data_0(data):
+    hourly_data = data["hourly"]
+    for time, temperature, humidity, wind_speed in zip(
+        hourly_data["time"],
+        hourly_data["temperature_2m"],
+        hourly_data["relative_humidity_2m"],
+        hourly_data["wind_speed_10m"],
+    ):
+        date_time = datetime.fromisoformat(time)
+        entry, created = WeatherEntry.objects.get_or_create(
+            date_time=date_time,
+            defaults={
+                "temperature": temperature,
+                "relative_humidity": humidity,
+                "wind_speed": wind_speed,
+            },
+        )
+        # Handle potential updates here if needed
+
+
+def process_and_store_data(data):
+    hourly_data = data["hourly"]
+    for time, temperature, humidity, wind_speed in zip(
+        hourly_data["time"],
+        hourly_data["temperature_2m"],
+        hourly_data["relative_humidity_2m"],
+        hourly_data["wind_speed_10m"],
+    ):
+        date_time = datetime.fromisoformat(time)
+        date_only = date_time.date()
+
+        # Create or update WeatherEntry
+        entry, created = WeatherEntry.objects.get_or_create(
+            date_time=date_time,
+            defaults={
+                "temperature": temperature,
+                "relative_humidity": humidity,
+                "wind_speed": wind_speed,
+            },
+        )
+
+        # Calculate daily average temperature
+        try:
+            daily_summary = DailyWeatherSummary.objects.get(date=date_only)
+        except DailyWeatherSummary.DoesNotExist:
+            daily_summary = DailyWeatherSummary(date=date_only)
+        daily_summary.average_temperature = WeatherEntry.objects.filter(
+            date_time__date=date_only
+        ).aggregate(Avg("temperature"))["temperature__avg"]
+        daily_summary.save()
